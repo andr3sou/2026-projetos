d3c6a1814a5fc8f080d0ddcf57a679889f3aaacc
André Santos de Souza <andresou688@gmail.com>
2026-02-11 14:39:14 -0300

    1

---
+++ Vs code/script.js
@@ -0,0 +1,31 @@
+//Tipos de dado:
+
+//Texto
+let nome= "Maria";
+let nome2= "Andre";
+
+console.log("Me chamo" + nome + "  " + nome2);
+console.log(nome2);
+
+//Número
+let num = "35";
+
+console.log(num);
+console.log("Minha idade é:" + num);
+
+//booleano
+let booleano=true;
+console.log("maior que 18 anos:" + booleano);
+
+//null e underfined
+let testeNull=null;
+let testeUndefined;
+
+console.log(testeNull);
+console.log(testeUndefined);
+console.log(testeUndefined * 2);
+
+console.log("6" + 4);
+console.log("6" - 4);
+console.log("6" * 4);
+console.log("6" / 4);
