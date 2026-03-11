01d8f0812fd284e03de871aaaf2998cfdbffaaf8
André <andresou688@gmail.com>
2026-03-11 16:40:41 -0300

    Add files via upload
    Site com Java script

---
+++ JavaScript(Site)/script.js
@@ -0,0 +1,13 @@
+let h1 = document.getElementById("title");
+console.log(h1);
+
+h1.textContent = "LABUBU";
+h1.style.color = "blue";
+
+let p = document.querySelector(".paragrafo");
+console.log(p);
+
+p.textContent = "BAhhhhh";
+
+let div = document.getElementById("background");
+div.style.backgroundColor = "purple";
