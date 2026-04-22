1446cde9edad4f0d360e8344485025661497bc4e
André <andresou688@gmail.com>
2026-04-22 16:37:23 -0300

    Update script.js

---
+++ carrinho de compra(JS)/script.js
@@ -0,0 +1,16 @@
+const produtosLoja = [
+    { id: 1, nome: "notebook", preco: 300 },
+    { id: 2, nome: "mouse", preco: 100 },
+    { id: 3, nome: "teclado", preco: 140 }  
+];
+
+let produtosCarrinho = [];
+
+function renderizarProduto(){
+    let lista = document.getElementById("lista-produtos");
+    lista.innerHTML = " ";
+
+    produtosLoja.forEach(produto => {
+        
+    })
+};
